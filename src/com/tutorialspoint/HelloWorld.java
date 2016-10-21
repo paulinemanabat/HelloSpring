202edc9f86b0ee44ab10e7eda64ae6c5e9d01370
paulinemanabat <paulinerosemanabat@gmail.com>
2016-10-21 15:02:31 +0800

    hello spring

---
+++ src/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.tutorialspoint;
+
+public class HelloWorld {
+	private String message;
+	private String message2;
+
+	public void getMessage() {
+		System.out.println("Your message :" + message);
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage2() {
+		System.out.println("Your message :" + message2);
+	}
+
+	public void setMessage2(String message2) {
+		this.message2 = message2;
+	}
+
+}
